9450fe6529b6fa1bfe56b174e40a1d22a73b04f6
JCa12s <joeldec57@gmail.com>
2026-07-13 14:26:15 +0100

    segundo_estagio

--- main.c
+++ main.c
@@ -1,40 +1,71 @@
-#include <unistd.h>
+#include <stdio.h>
 
 //Fase inicialização das funções de operações matemáticas
 // Função de adição
 void adicao(int a, int b)
 {
-    return a + b;
+    int resultado = a + b;
+    printf("Resultado: %d\n", resultado);
+    return;
 }
 
 // Função de subtração
 void subtracao(int a, int b)
 {
-    return a - b;
+    int resultado = a - b;
+    printf("Resultado: %d\n", resultado);
+    return;
 }
 
 // Função de multiplicação
 void multiplicacao(int a, int b)
 {
-    return a * b;
+    int resultado = a * b;
+    printf("Resultado: %d\n", resultado);
+    return;
 }
 
 // Função de divisão
 void divisao(int a, int b)
 {
-    return a / b;
+    int resultado = a / b;
+    printf("Resultado: %d\n", resultado);
+    return;
 }
 
 // Fase de execução do programa
 int main()
 {
-    int a = 10;
-    int b = 5;
+    int a, b, opcao;
+    printf("Calculadora Simples\n");
+    printf("Escolha uma operação:\n");
+    printf("1. Adição\n");
+    printf("2. Subtração\n");
+    printf("3. Multiplicação\n");
+    printf("4. Divisão\n");
+    scanf("%d", &opcao);
+    printf("Digite o primeiro número: ");
+    scanf("%d", &a);
+    printf("Digite o segundo número: ");
+    scanf("%d", &b);
 
-    adicao(a, b);
-    subtracao(a, b);
-    multiplicacao(a, b);
-    divisao(a, b);
+    switch(opcao)
+    {
+        case 1:
+            adicao(a, b);
+            break;
+        case 2:
+            subtracao(a, b);
+            break;
+        case 3:
+            multiplicacao(a, b);
+            break;
+        case 4:
+            divisao(a, b);
+            break;
+        default:
+            printf("Opção inválida!\n");
+    }
 
     return 0;
 }
