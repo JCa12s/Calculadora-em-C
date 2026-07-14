394d354e43af11379b5522bc0283a858668b279e
JCa12s <joeldec57@gmail.com>
2026-07-14 14:35:43 +0100

    new_push

--- main.c
+++ main.c
@@ -2,41 +2,43 @@
 
 //Fase inicialização das funções de operações matemáticas
 // Função de adição
-void adicao(int a, int b)
+void adicao(float a, float b)
 {
-    int resultado = a + b;
-    printf("Resultado: %d\n", resultado);
+    float resultado = a + b;
+    printf("Resultado: %0.2f\n", resultado);
     return;
 }
 
 // Função de subtração
-void subtracao(int a, int b)
+void subtracao(float a, float b)
 {
-    int resultado = a - b;
-    printf("Resultado: %d\n", resultado);
+    float resultado = a - b;
+    printf("Resultado: %0.2f\n", resultado);
     return;
 }
 
 // Função de multiplicação
-void multiplicacao(int a, int b)
+void multiplicacao(float a, float b)
 {
-    int resultado = a * b;
-    printf("Resultado: %d\n", resultado);
+    float resultado = a * b;
+    printf("Resultado: %0.2f\n", resultado);
     return;
 }
 
 // Função de divisão
-void divisao(int a, int b)
+void divisao(float a, float b)
 {
-    int resultado = a / b;
-    printf("Resultado: %d\n", resultado);
+    float resultado = a / b;
+    printf("Resultado: %0.2f\n", resultado);
     return;
 }
 
 // Fase de execução do programa
 int main()
 {
-    int a, b, opcao;
+    float a, b;
+    int opcao;
+    
     printf("Calculadora Simples\n");
     printf("Escolha uma operação:\n");
     printf("1. Adição\n");
@@ -45,9 +47,9 @@ int main()
     printf("4. Divisão\n");
     scanf("%d", &opcao);
     printf("Digite o primeiro número: ");
-    scanf("%d", &a);
+    scanf("%f", &a);
     printf("Digite o segundo número: ");
-    scanf("%d", &b);
+    scanf("%f", &b);
 
     switch(opcao)
     {
