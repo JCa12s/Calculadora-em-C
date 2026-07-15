876a66235471e4ed413b8ca6fbd00cd88b3d3640
JCa12s <joeldec57@gmail.com>
2026-07-15 22:36:27 +0100

    Novo_arquivo_para_o_organização_no_geral

---
+++ funcoes.h
@@ -0,0 +1,10 @@
+#ifndef FUNCOES_H
+#define FUNCOES_H
+
+// Retirar as funcoes de operações matemáticas do main.c e colocar em arquivos separados.
+void adicao(float a, float b);
+void subtracao(float a, float b);
+void multiplicacao(float a, float b);
+void divisao(float a, float b);
+
+#endif
